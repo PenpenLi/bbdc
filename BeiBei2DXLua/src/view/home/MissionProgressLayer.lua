80eb34ffec324df847b4f0eb7fcdfab4dd358ee8
YeHanjie <omega_ztz@126.com>
2015-05-07 13:32:48 +0800

    delete test code

--- BeiBei2DXLua/src/view/home/MissionProgressLayer.lua
+++ BeiBei2DXLua/src/view/home/MissionProgressLayer.lua
@@ -197,8 +197,8 @@ function MissionProgressLayer.create(share,homelayer)
             if s_CURRENT_USER.newTutorialStep == s_newtutorial_story then
                 s_CorePlayManager.enterStoryLayer()
             else
-                --s_CorePlayManager.enterLevelLayer()
-                s_CorePlayManager.enterStoryLayer()
+                s_CorePlayManager.enterLevelLayer()
+                --s_CorePlayManager.enterStoryLayer()
 
             end
         end)
